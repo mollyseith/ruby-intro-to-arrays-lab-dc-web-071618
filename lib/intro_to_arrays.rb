823ef929cf7b395e5618350399927b33cadc525a
Molly Seither <molly.seither@centre.edu>
2018-07-12 14:45:48 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  ret = []
+  return ret
+end
+
+def array_with_two_elements
+  ret = ["",""]
+  return ret
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  return array.shift
+end
+
+def last_element_with_array_methods(array)
+  return array.pop
+end
+
+def length_of_array(array)
+  return array.length
+end
